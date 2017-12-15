4fecf188ffe9b33c2fc2102e99170c0df462a612
yanjun <yanjunzan@gmail.com>
2017-12-15 13:20:29 +0100

    add this py scripts to screen  4 grand parents for fixed allele,and reformating vcf

---
+++ python/171215_read.vcf.grand.p.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+import re  # regular expression
+import sys
+import gzip
+#  import pandas
+
+ID_of = re.compile(str(sys.argv[1]))
+Fid_h = re.compile(str(sys.argv[2]))
+Mid_h = re.compile(sys.argv[3])
+Fid_l = re.compile(str(sys.argv[4]))
+Mid_l = re.compile(sys.argv[5])
+vcf_file = str(sys.argv[6])
+out_file = str(sys.argv[7])
+# ID_of = re.compile("175")
+# Fid = re.compile(".*1997.*")
+# Mid = re.compile(".*1812.*")
+# vcf_file = "/Users/yanjunzan/Documents/impute/data/testplate.F1/chr1_CM000093.4.recode.vcf.gz"
+sys.stdout = open(out_file, "wt")
+
+
+def find_index(name, lines):
+    indices = []
+    for i, elem in enumerate(lines):
+        if re.match(name, elem):
+            indices.append(i)
+    return indices
+
+
+def get_geno(x):
+    if re.match("^\.", x):
+        return [5, 5]
+    else:
+        out = re.match("(\d{1}).{1}(\d{1}).*", x)
+        return out.group(1, 2)
+
+
+with gzip.open(vcf_file, "rb") as vcf:
+    for line in vcf:
+        if re.match(b"^##", line):
+            pass
+        elif re.match(b"^#CHROM", line):
+            line = line.decode().rstrip("\n")
+            header = re.split("\t", line)
+            index_off = find_index(ID_of, header)
+            index_F_h = find_index(Fid_h, header)
+            index_M_h = find_index(Mid_h, header)
+            index_F_l = find_index(Fid_l, header)
+            index_M_l = find_index(Mid_l, header)
+            #  print(index_off, index_F, index_M)
+        else:
+            line = line.decode().rstrip("\n")
+            con = re.split("\t", line)
+            Chr = con[0]
+            Pos = con[1]
+            ID = con[2]
+            REF = con[3]
+            ALT = con[4]
+            if len(REF) > 1 or len(ALT) > 1:
+                pass
+            elif re.match("\.", con[index_off[0]]):
+                pass
+            elif re.match("\.", con[index_F_h[0]]):
+                pass
+            elif re.match("\.", con[index_M_h[0]]):
+                pass
+            elif re.match("\.", con[index_M_l[0]]):
+                pass
+            elif re.match("\.", con[index_M_l[0]]):
+                pass
+            else:
+                geno_of1 = int(get_geno(con[index_off[0]])[0])
+                geno_of2 = int(get_geno(con[index_off[0]])[1])
+                geno_F1_h = int(get_geno(con[index_F_h[0]])[0])
+                geno_F2_h = int(get_geno(con[index_F_h[0]])[1])
+                geno_M1_h = int(get_geno(con[index_M_h[0]])[0])
+                geno_M2_h = int(get_geno(con[index_M_h[0]])[1])
+                geno_F1_l = int(get_geno(con[index_F_l[0]])[0])
+                geno_F2_l = int(get_geno(con[index_F_l[0]])[1])
+                geno_M1_l = int(get_geno(con[index_M_l[0]])[0])
+                geno_M2_l = int(get_geno(con[index_M_l[0]])[1])
+                if sum([geno_F1_h, geno_F2_h, geno_M1_h, geno_M2_h,geno_F1_l, geno_F2_l, geno_M1_l, geno_M1_l]) == 4 and (sum([geno_F1_h, geno_F2_h, geno_M1_h, geno_M1_h]) == 0 or sum([geno_F1_h, geno_F2_h, geno_M1_h, geno_M1_h]) == 4 ):
+                    print(Chr, Pos, REF, ALT, geno_F1_h, geno_M2_h, geno_F1_l, geno_M2_l, geno_of1, geno_of2, sep="\t")
